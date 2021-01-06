64bce18874a621de8502a4823ecdbcabd2c8789d
Kelly Thomas <kellydye77@gmail.com>
2021-01-06 15:56:06 -0600

    Added Data Not Availalbe to ToString if a field is emptor null

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -32,6 +32,27 @@ namespace TechJobsOO
 
         public override string ToString()
         {
+            if (Name == "")
+            {
+                Name = "Data not available";
+            }
+
+            if (EmployerName.Value == "" || EmployerName.Value == null)
+            {
+                EmployerName.Value = "Data not available";
+            }
+            if (EmployerLocation.Value == "" || EmployerLocation.Value == null)
+            {
+                EmployerLocation.Value = "Data not available";
+            }
+            if (JobType.Value == "" || JobType.Value == null)
+            {
+                JobType.Value = "Data not available";
+            }
+            if (JobCoreCompetency.Value == "" || JobCoreCompetency.Value == null)
+            {
+                JobCoreCompetency.Value = "Data not available";
+            }
             string output = $"ID:{Id} \n Name:{Name} \n Employer:{EmployerName.Value} \n Location:{EmployerLocation.Value} \n Position Type:{JobType.Value} \n Core Competency:{JobCoreCompetency.Value}";
             return output;
         }
