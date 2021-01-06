f03a9e3d648fe4d97927a7230d36c8ed2c277430
Kelly Thomas <kellydye77@gmail.com>
2021-01-06 16:25:05 -0600

    Dried code in Position, Employer, CoreComp, and Location- FINISHED!!!!

--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -1,39 +1,11 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class CoreCompetency
+    public class CoreCompetency : JobField
     {
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
-
-        // TODO: Change the fields to auto-implemented properties.*****DONE*********
-
-        public CoreCompetency()
+        public CoreCompetency(string value) : base(value)
         {
-            Id = nextId;
-            nextId++;
-        }
 
-        public CoreCompetency(string value) : this()
-        {
-            Value = value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   Id == competency.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
         }
     }
 }
