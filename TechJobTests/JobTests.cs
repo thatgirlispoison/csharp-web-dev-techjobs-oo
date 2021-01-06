f55cc45e4538481b377b55eb77d643d7e627eaea
Kelly Thomas <kellydye77@gmail.com>
2021-01-06 14:50:58 -0600

    Completed and passed Test 4 TestJobsForEquality

--- TechJobTests/JobTests.cs
+++ TechJobTests/JobTests.cs
@@ -9,6 +9,8 @@ namespace TechJobTests
     {
         Job testJob1 = new Job();
         Job testJob2 = new Job();
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality"), new CoreCompetency("Persistence"));
+        Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality"), new CoreCompetency("Persistence"));
 
         [TestMethod]
         public void TestSettingJobId()
@@ -18,12 +20,17 @@ namespace TechJobTests
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
-            Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality"), new CoreCompetency("Persistence"));
             Assert.IsTrue(testJob3.Name == "Product tester");
             Assert.IsTrue(testJob3.EmployerName.Value == "ACME");
             Assert.IsTrue(testJob3.EmployerLocation.Value == "Desert");
             Assert.IsTrue(testJob3.JobType.Value == "Quality");
             Assert.IsTrue(testJob3.JobCoreCompetency.Value == "Persistence");
+        }
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(testJob3.Equals(testJob4));
+            Assert.IsFalse(testJob3.Id == testJob4.Id );
         }
     }
 }
