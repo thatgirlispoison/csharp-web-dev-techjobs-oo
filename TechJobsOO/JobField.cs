c5a5c49b8d1e04db2af3d4b1479383c3ef1cfd6e
Kelly Thomas <kellydye77@gmail.com>
2021-01-06 16:17:23 -0600

    Added JobField class and added functionality

---
+++ TechJobsOO/JobField.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechJobsOO
+{
+    public abstract class JobField
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
+
+        public JobField()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobField field &&
+                   Id == field.Id &&
+                   Value == field.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Value);
+        }
+    }
+}
